626078b8c31eae98dc6242ef923acfcb62eb4dec
Alexandre Hallaine <hallainealexandre@gmail.com>
2023-02-27 12:47:52 +0100

    Update: main display

--- src/main.cpp
+++ src/main.cpp
@@ -48,20 +48,20 @@ int main(int argc, char **argv)
 			  << *puzzle << std::endl
 			  << std::endl;
 
-	std::cout << "Solving..." << std::endl
-			  << std::endl;
-
+	std::cout << "Solving..." << std::endl;
 	AStarSearch search(std::make_unique<ManhattanDistance>());
 
 	auto start = std::chrono::steady_clock::now();
 	std::unique_ptr<std::vector<Puzzle>> path = search.solve(*puzzle);
 	auto end = std::chrono::steady_clock::now();
 
+	if (path == nullptr)
+		std::cout << "No solution found" << std::endl;
+	else
+		std::cout << "Solution found in " << path->size() - 1 << " moves" << std::endl;
+	std::cout << std::endl;
+
 	std::chrono::duration<double> elapsed_seconds = end - start;
 	std::cout << "Elapsed time: " << elapsed_seconds.count() << "s" << std::endl
 			  << search;
-
-	if (path == nullptr)
-		error("No solution found", 1);
-	std::cout << "Solution found in " << path->size() - 1 << " moves" << std::endl;
 }
